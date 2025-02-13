88e2a6f4f3aacca42d9be817e0c5de8e7da4915e
cako <cakoonrails@gmail.com>
2025-02-13 16:46:01 -0300

    adding player sprite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 from constants import *
-
+from player import *
 
 def main():
     pygame.init()
@@ -8,11 +8,13 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    player = Player(x = SCREEN_WIDTH /2, y = SCREEN_HEIGHT / 2)
+
     while True:
         pygame.Surface.fill(screen, color="black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
-
 
 
 if __name__ == "__main__":
